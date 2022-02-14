14fb895442d66e3ab461eefec4b2604e050773e2
Kelsey Crow <kelseybcrow@gmail.com>
2022-02-13 17:46:25 -0700

    got duel duo up

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 const express = require('express')
+const cors = require('cors')
 const path = require('path')
-const app = express()
-const {bots, playerRecord} = require('./data')
-const {shuffleArray} = require('./utils')
 
+const app = express()
 app.use(express.json())
+app.use(cors())
+app.use(express.static('public'))
+
+const { bots, playerRecord } = require('./data')
+const { shuffleArray } = require('./utils')
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'))
+})
+
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../public/index.css'))
+// })
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../public/index.js'))
+// })
 
 app.get('/api/robots', (req, res) => {
     try {
@@ -20,7 +35,7 @@ app.get('/api/robots/five', (req, res) => {
         let shuffled = shuffleArray(bots)
         let choices = shuffled.slice(0, 5)
         let compDuo = shuffled.slice(6, 8)
-        res.status(200).send({choices, compDuo})
+        res.status(200).send({ choices, compDuo })
     } catch (error) {
         console.log('ERROR GETTING FIVE BOTS', error)
         res.sendStatus(400)
@@ -30,16 +45,24 @@ app.get('/api/robots/five', (req, res) => {
 app.post('/api/duel', (req, res) => {
     try {
         // getting the duos from the front end
-        let {compDuo, playerDuo} = req.body
+        let { compDuo, playerDuo } = req.body
 
         // adding up the computer player's total health and attack damage
         let compHealth = compDuo[0].health + compDuo[1].health
-        let compAttack = compDuo[0].attacks[0].damage + compDuo[0].attacks[1].damage + compDuo[1].attacks[0].damage + compDuo[1].attacks[1].damage
-        
+        let compAttack =
+            compDuo[0].attacks[0].damage +
+            compDuo[0].attacks[1].damage +
+            compDuo[1].attacks[0].damage +
+            compDuo[1].attacks[1].damage
+
         // adding up the player's total health and attack damage
         let playerHealth = playerDuo[0].health + playerDuo[1].health
-        let playerAttack = playerDuo[0].attacks[0].damage + playerDuo[0].attacks[1].damage + playerDuo[1].attacks[0].damage + playerDuo[1].attacks[1].damage
-        
+        let playerAttack =
+            playerDuo[0].attacks[0].damage +
+            playerDuo[0].attacks[1].damage +
+            playerDuo[1].attacks[0].damage +
+            playerDuo[1].attacks[1].damage
+
         // calculating how much health is left after the attacks on each other
         let compHealthAfterAttack = compHealth - playerAttack
         let playerHealthAfterAttack = playerHealth - compAttack
@@ -70,5 +93,5 @@ app.get('/api/player', (req, res) => {
 const port = process.env.PORT || 3000
 
 app.listen(port, () => {
-  console.log(`Listening on port ${port}`)
+    console.log(`Listening on port ${port}`)
 })
