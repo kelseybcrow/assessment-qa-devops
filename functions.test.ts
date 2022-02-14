bcd5311cc054101702619b272a0cc7e8b2bc9b6f
Kelsey Crow <kelseybcrow@gmail.com>
2022-02-13 23:10:43 -0700

    added part 3 tests

--- functions.test.ts
+++ functions.test.ts
@@ -1,5 +1,12 @@
-const {shuffleArray} = require('./utils')
+const { shuffleArray } = require('./utils')
 
 describe('shuffleArray should', () => {
     // CODE HERE
+    test('be an array', () => {
+        expect([12, 2]).toEqual([12, 2])
+    })
+
+    test('have the same array length', () => {
+        expect([1, 2, 3]).toHaveLength(3)
+    })
 })
